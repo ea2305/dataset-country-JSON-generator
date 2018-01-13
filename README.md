b829a21683c4c718bbbf5ccb33b51703f6751b67
Elihu Cruz <ea230596@gmail.com>
2018-01-12 23:07:53 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Country Dataset generator (Country + State + City) 🌎
+
+Simple dataset generator. Find, Select and use.
+
+## How to use ?
+
+Change the "serach" country name inside datase.js and
+
+### License
+MIT
---
+++ dataset.js
@@ -0,0 +1,38 @@
+'use strict'
+/**
+ * Dataset generator
+ * @author Elihu A. Cruz
+ * @version 1.0.1
+ */
+
+const fs = require('fs')
+
+// Load base dataset
+let countries = JSON.parse(fs.readFileSync('./countries.json','utf8')).countries
+let states    = JSON.parse(fs.readFileSync('./states.json','utf8')).states
+let cities    = JSON.parse(fs.readFileSync('./cities.json','utf8')).cities
+
+// Seleccionamos pais de busqueda
+let search = 'Mexico'
+
+// get country
+let country = countries.find(country => country.name === search)
+
+// get country states
+country.states = states.filter(state => (state.country_id === country.id)? state : null)
+
+// get cities
+country.states.map(state => {
+  state.cities  = cities.filter(city => (city.state_id === state.id)? city : null)
+})
+
+// write new dataset
+fs.writeFile("./dataset/full_country.json", JSON.stringify(country), function(err) {
+    if(err)
+      return console.log(err)
+
+    console.log("The file was saved!");
+})
+
+// show me :p
+console.log(country);
